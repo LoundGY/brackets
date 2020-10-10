85fa86c6161d4c2d1d5bd7f4b6b96c962d3df311
LoundGY <44819298+LoundGY@users.noreply.github.com>
2020-10-10 13:08:46 +0300

    fix: fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let open = [];
+    let close = [];
+    let stack = [];
+    let result;
+
+    for (let i = 0; i < bracketsConfig.length; i++){
+        open.push(bracketsConfig[i][0]);
+        close.push(bracketsConfig[i][1]);
+    }
+
+    for (let i = 0; i < str.length; i++){
+        if (open.includes(str[i]) && close.indexOf(str[i]) === -1 || open.includes(str[i]) && close.includes(str[i]) && stack[stack.length - 1] !== str[i]) {
+            stack.push(str[i]);
+        }
+        else if (open.indexOf(stack[stack.length - 1]) === close.indexOf(str[i])) {
+            stack.pop();
+        }
+        else {
+            result = false;
+            stack.push(str[i]);
+            break;
+        }
+    }
+    if (stack.length === 0) {
+        result = true;
+    }
+    else {
+        result = false;
+    }
+    return result;
+
 }
